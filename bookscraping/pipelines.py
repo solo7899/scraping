b71dfeb4d13f4ebb239c10865811547508029a4c
hamidreza <dolati.hamidreza78@gmail.com>
2023-05-11 21:04:45 +0330

    set the pipline

--- bookscraping/pipelines.py
+++ bookscraping/pipelines.py
@@ -10,4 +10,15 @@ from itemadapter import ItemAdapter
 
 class BookscrapingPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        for field_name in adapter.field_names():
+            if field_name != "description":
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+
+        value = adapter.get("price")
+        value = value.replace("£", "")
+        adapter["price"] = float(value)
+
         return item
